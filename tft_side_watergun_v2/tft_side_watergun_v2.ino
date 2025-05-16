8407ad870ceb591902d0ba818f76c70d5f37c985
jonathanrandall <jonnyrandall@live.com.au>
2025-05-16 20:43:29 +1000

    new code

--- tft_side_watergun_v2/tft_side_watergun_v2.ino
+++ tft_side_watergun_v2/tft_side_watergun_v2.ino
@@ -6,9 +6,6 @@
 
 
 
-// const char *ssid = "ESP32-CAM-AP";  // SSID of the ESP32-CAM AP
-// const char *password = "123456789";  // Password for the AP
-
 
 // const char* stream_url ="http://jonny-ubuntu.local:81/";// "http://192.168.4.1:81/stream";
 // const char* stream_url ="http://192.168.4.1:81/stream";
@@ -314,144 +311,11 @@ void loop() {
     
     delay(50);
 
-  //button stuff
-  // if (millis() - last_read > 1000 && digitalRead(laser_button)){
-  //   digitalWrite(laser, !digitalRead(laser));
-  //   last_read = millis();
-  // }
-
-  // if (!fire_done && digitalRead(fire_button)){
-  //   spray_target_with_water();
-  //   // sendMessage("target_get");
-  // }
-  
-
-  // if (stream && stream->connected()) {
-  //   static bool in_image = false;
-  //   static size_t content_length = 0;
-  //   static uint8_t* imageBuffer = nullptr;
-
-  //   if (stream->available()) {
-  //     String line = stream->readStringUntil('\n');
-
-  //     if (line.startsWith("--")) {
-  //       in_image = false;
-  //       if (imageBuffer) {
-  //         drawJpeg(imageBuffer, content_length);
-  //         free(imageBuffer);
-  //         imageBuffer = nullptr;
-  //       }
-  //       content_length = 0;
-  //     }
-
-  //     if (line.startsWith("Content-Length: ")) {
-  //       content_length = line.substring(15).toInt();
-  //       imageBuffer = (uint8_t*)malloc(content_length);
-  //       in_image = true;
-  //     }
-
-  //     if (in_image && imageBuffer) {
-  //       size_t remaining = content_length;
-  //       size_t bytes_read = 0;
-
-  //       while (remaining > 0 && stream->available()) {
-  //         bytes_read += stream->readBytes(imageBuffer + bytes_read, remaining);
-  //         remaining = content_length - bytes_read;
-  //       }
-
-  //       if (remaining == 0) {
-  //         in_image = false;
-  //       }
-  //     }
-  //   }
-  // } else {
-  //   Serial.println("Stream disconnected!");
-  //   http.end();
-  //   http.begin(stream_url);
-  //   int httpCode = http.GET();
-  //   if (httpCode == HTTP_CODE_OK) {
-  //     WiFiClient * stream = http.getStreamPtr();
-  //   }
-  // }
-
-  // delay(10);
-
-  ///////////////////////////////////////
-  // delay(1000);
-  //  if (WiFi.status() == WL_CONNECTED) {
-  //   HTTPClient http;
-    
-  //   // Start the HTTP connection to the ESP32-CAM stream
-  //   http.begin(stream_url);
-  //   int httpCode = http.GET();
-
-  //   if (httpCode == HTTP_CODE_OK) {
-  //     WiFiClient * stream = http.getStreamPtr();
-  //     bool in_image = false;
-  //     size_t content_length = 0;
-  //     uint8_t *imageBuffer = nullptr;
-
-  //     while (stream->connected()) {
-  //       if (stream->available()) {
-  //         String line = stream->readStringUntil('\n');
-
-  //         // Check for boundary marker
-  //         if (line.startsWith("--")) {
-  //           in_image = false;
-  //           if (imageBuffer) {
-  //             // Display the received image on the TFT
-  //             drawJpeg(imageBuffer, content_length);
-  //             free(imageBuffer);
-  //             imageBuffer = nullptr;
-  //           }
-  //           content_length = 0;  // Reset the content length for the next image
-  //         }
-
-  //         // Check if the line is the content length header
-  //         if (line.startsWith("Content-Length: ")) {
-  //           content_length = line.substring(15).toInt();  // Use 16 when ends with a semi-colon
-  //           Serial.printf("Content-Length: %d\n", content_length);
-  //           imageBuffer = (uint8_t *)malloc(content_length);  // Allocate memory for the image
-  //           in_image = true;  // Indicate that the image is coming
-  //         }
-
-  //         // If we're in the image data, read it into the buffer
-  //         if (in_image && imageBuffer) {
-  //           size_t remaining = content_length;
-  //           size_t bytes_read = 0;
-
-  //           while (remaining > 0 && stream->available()) {
-  //             bytes_read += stream->readBytes(imageBuffer + bytes_read, remaining);
-  //             remaining = content_length - bytes_read;
-  //           }
-
-  //           if (remaining == 0) {
-  //             //Serial.println("Image received!");
-  //             in_image = false;  // Image is fully received
-  //           }
-  //         }
-  //       }
-  //     }
-  //   } else {
-  //     Serial.printf("HTTP GET failed, error: %d\n", httpCode);
-  //   }
-
-  //   http.end(); // Close the HTTP connection
-  // }
-
-  // // delay(5000); // Adjust the delay as necessary for the next request
-  // delay(20);
+ 
 }
 
 
 
-// void displayImage(uint8_t *imgData, int length) {
-//   // Assuming imgData is in JPEG format and your TFT library can handle it
-//   // You may need to implement a function that converts and displays the JPEG data
-//   tft.setRotation(0);
-//   tft.pushImage(0, 0, 320, 240, imgData);  // Adjust dimensions as needed
-//   Serial.println("Image displayed on TFT");
-// }
 
 void image_stream(void *args){
   HTTPClient http;
